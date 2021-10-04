6660ba01ffd0a9fbb875aa79f3726ce3ed6dfaae
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-10-04 17:04:11 -0400

    A list is not a function

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for domain in typodomains:
     req = requests.get("http://{}".format(domain),headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8","Accept-Encoding":"gzip, deflate, br","Accept-Language":"en-US,en;q=0.5","DNT":"1"})
     if urlparse(req.url).netloc not in safeends:
       alivedomains.append(domain)
-      enddomains(urlparse(req.url).netloc)
+      enddomains.append(urlparse(req.url).netloc)
       for reddomain in req.history:
         redirecturls.append(reddomain)
       redirecturls.append(req.url)
