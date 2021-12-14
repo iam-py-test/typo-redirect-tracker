8d4bc4a638ff7c63a56ed835b586f48e79f7b655
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-12-14 15:44:41 -0500

    Create screenshot.py

---
+++ screenshot.py
@@ -0,0 +1,24 @@
+# https://faun.pub/how-to-install-selenium-in-linux-e8928b2b709
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import datetime
+
+# Set path Selenium
+CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
+s = Service(CHROMEDRIVER_PATH)
+WINDOW_SIZE = "1920,1080"
+
+# Options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(service=s, options=chrome_options)
+
+dt = datetime.datetime.now()
+
+for domain in open("alivedomains.txt"):
+  driver.get("http://{}".format(domain))
+  driver.save_screenshot("{}_screenshot.png".format(domain))
+driver.close()
